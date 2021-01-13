85035ceb7747c156324e9fe40680894f86c3d933
Yamingue <yamking01@gmail.com>
2021-01-13 11:13:07 +0100

    listing des chambre par city

--- src/Form/ChambreType.php
+++ src/Form/ChambreType.php
@@ -4,7 +4,6 @@ namespace App\Form;
 
 use App\Entity\Chambre;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -37,6 +36,7 @@ class ChambreType extends AbstractType
                 ]
             ])
             ->add('type')
+            ->add('prix')
             ->add('description')
             ->add('poster',FileType::class,[
                 'constraints'=>[
